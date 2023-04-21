4c09bba14a61185bb8494d7ad194a7198d337a60
manipindi <manikanta.pindi@narayanagroup.com>
2023-04-21 12:02:28 +0530

    favicon and title change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+// import fav from '../public/favicon.svg'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Mani Pindi</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

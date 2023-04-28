2a586b8b3d9d85ec09a5f4b7985aaaddc0562a06
manipindi <manikanta.pindi@narayanagroup.com>
2023-04-28 11:34:15 +0530

    responsive

--- shared/molecules/work-card/index.tsx
+++ shared/molecules/work-card/index.tsx
@@ -5,24 +5,25 @@ const CardWrap: any = styled.div`
   width: 680px;
   height: 620px;
   background-size: cover;
-  
-  ${({bgposition}:any)=>bgposition && css`
-    background-position: ${bgposition};
-  `}
 
-  ${({bgImg}:any)=>bgImg && css`
-    background-image: url(${bgImg?.src});
+  ${({ bgposition }: any) =>
+    bgposition &&
+    css`
+      background-position: ${bgposition};
+    `}
 
-  `}
+  ${({ bgImg }: any) =>
+    bgImg &&
+    css`
+      background-image: url(${bgImg?.src});
+    `}
 
-  @media screen and (max-width: 420px){
+  @media screen and (max-width: 420px) {
     width: 100%;
     height: 360px;
   }
 `;
 
 export default function WorkCard({ img, bgposition }: any) {
-  console.log(img);
-  
   return <CardWrap bgposition={bgposition} bgImg={img}></CardWrap>;
 }
